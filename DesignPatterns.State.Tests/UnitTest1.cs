07ef660c838d633983d888a6cdaa48bf1639c71f
petro kolosov <kolosovp94@gmail.com>
2020-09-25 00:42:43 +0300

    initial commit

---
+++ DesignPatterns.State.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace DesignPatterns.State.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

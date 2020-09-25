54692dd816168a2158ec6abafb65313a8e2ac040
petro kolosov <kolosovp94@gmail.com>
2020-09-25 13:57:34 +0300

    tread safe singleton implemented

---
+++ DesignPatterns.Singleton.Tests/SingletonTests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DesignPatterns.Singleton.Tests.SingletonTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Singleton_Test()
+        {
+            var instance1 = Concrete.Singleton.GetInstance;
+            var instance2 = Concrete.Singleton.GetInstance;
+            instance1.TimeCreated.Should().Be(instance2.TimeCreated);
+            ReferenceEquals(instance1, instance2).Should().BeTrue();
+        }
+    }
+}
---
+++ DesignPatterns.Singleton/Concrete/Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton.Concrete
+{
+    public class Singleton
+    {
+        public  DateTime TimeCreated { get; }
+
+        public static Singleton GetInstance { get; } = new Singleton();
+
+        static Singleton()
+        {
+        }
+
+        private Singleton()
+        {
+            TimeCreated = DateTime.Now;
+        }
+    }
+}

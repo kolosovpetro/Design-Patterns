2064328c59ca2c4a1248a264814647613027d6f8
petro kolosov <kolosovp94@gmail.com>
2020-09-26 06:19:00 +0300

    chain of responsibility test added

---
+++ DesignPatterns.ChainOfResponsibility.Tests/Tests/ChainTest.cs
@@ -0,0 +1,32 @@
+﻿using DesignPatterns.ChainOfResponsibility.Handlers;
+using DesignPatterns.ChainOfResponsibility.Interfaces;
+using DesignPatterns.ChainOfResponsibility.Requests;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DesignPatterns.ChainOfResponsibility.Tests.Tests
+{
+    [TestFixture]
+    public class ChainTest
+    {
+        [Test]
+        public void Chain_Test()
+        {
+            IChain chain = new Chain.Chain();
+            IHandler getHandler = new GetHandler();
+            IHandler putHandler = new PutHandler();
+            IHandler postHandler = new PostHandler();
+            IRequest request = new PutRequest(3);
+            
+            chain.AddNext(putHandler);
+            chain.AddNext(getHandler);
+            chain.AddNext(postHandler);
+            
+            chain.Handle(request);
+            request.IsHandled.Should().BeTrue();
+            getHandler.Response.Should().Be("Get Handler: Request with 3 is passed to next handler");
+            postHandler.Response.Should().Be("Post Handler: Request with 3 is passed to next handler");
+            putHandler.Response.Should().Be("Put Handler: Request with 3 is handled");
+        }
+    }
+}

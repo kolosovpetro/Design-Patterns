1fd90dc4a9e0aa07c8f1bce69b377cd207709e32
petro kolosov <kolosovp94@gmail.com>
2020-09-26 13:08:59 +0300

    iterator implemented

---
+++ DesignPatterns.Iterator.UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesignPatterns.Iterator.UI
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var collection = new[] {1, 2, 3, 4, 5, 6, 7};
+            var iterator = new Implementation.Iterator(collection);
+            
+            while (iterator.MoveNext())
+            {
+                Console.Write(iterator.Current + " ");
+            }
+        }
+    }
+}
---
+++ DesignPatterns.Iterator/Implementation/Iterator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+
+namespace DesignPatterns.Iterator.Implementation
+{
+    public class Iterator : IEnumerator
+    {
+        private readonly int[] _collection;
+        private int _index;
+
+        public Iterator(int[] collection)
+        {
+            _collection = collection;
+        }
+
+        public bool MoveNext()
+        {
+            return _index < _collection.Length;
+        }
+
+        public void Reset()
+        {
+            _index = 0;
+        }
+
+        public object Current => GetCurrent();
+
+        private object GetCurrent()
+        {
+            var currentIndex = _index;
+            _index++;
+            return _collection[currentIndex];
+        }
+    }
+}
